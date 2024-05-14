5759e0e20298c822a10c40d3eeb9280f815aa649
waterbom <159856504+waterbom@users.noreply.github.com>
2024-05-14 15:07:34 +0900

    123

--- front/src/App.js
+++ front/src/App.js
@@ -1,14 +1,16 @@
 import { Route, Routes } from "react-router-dom";
-import logo from "./logo.svg";
 import "./App.css";
 import Layout from "./layouts/Layout";
 import SignUp from "./pages/SignUp";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
     <Layout>
       <Routes>
+        <Route path="/" element={<SignUp />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </Layout>
   );
