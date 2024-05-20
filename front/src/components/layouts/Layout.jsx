64e2d37d109fdab2c134f459d497573dded775b2
SSU <kimsua903@gmail.com>
2024-05-20 09:50:00 +0900

    Footer page 완료

--- front/src/components/layouts/Layout.jsx
+++ front/src/components/layouts/Layout.jsx
@@ -1,16 +1,21 @@
 import { BrowserRouter, useLocation } from "react-router-dom";
 import Main from "./Main";
 import Header from "./Header";
+import Footer from "./Footer";
+
 
 const Layout = ({ children }) => {
   // 헤더를 숨길 페이지 경로 모음
   const location = useLocation();
   const hideHeaderPaths = ["/signup"];
+  const hideFooterPaths = ["/"];
+
 
   return (
     <>
-      {!hideHeaderPaths.includes(location.pathname) && <Header />}
+      {!hideHeaderPaths.includes(location.pathname) && <Header /> }
       <Main>{children}</Main>
+      {!hideFooterPaths.includes(location.pathname) && <Footer /> }
     </>
   );
 };
