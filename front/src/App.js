dd1ff3a189385511331d5d39c304b555c7303d04
LGY <leegyoung12@gmail.com>
2024-05-21 16:36:33 +0900

    Fix: 프로젝트 등록-DB 연결
    - 각 컬럼별 값 다 들어오도록 수정하였음
    - 라우터 대문자던거 소문자로 전부 변경

--- front/src/App.js
+++ front/src/App.js
@@ -8,6 +8,7 @@ import { useProvideAuth } from "./hooks/useProvideAuth";
 import AddMaker from "./pages/AddMaker";
 import AddProject from "./pages/AddProject";
 import SearchList from "./pages/SearchList";
+import Search from "./pages/Search";
 
 function App() {
   const auth = useProvideAuth();
@@ -18,10 +19,11 @@ function App() {
         <Routes>
           <Route path="/" element={<LoginPage />} />
           <Route path="/signup" element={<SignUp />} />
-          <Route path="/AddMaker" element={<AddMaker />} />
-          <Route path="/AddProject" element={<AddProject />} />
+          <Route path="/addmaker" element={<AddMaker />} />
+          <Route path="/addproject" element={<AddProject />} />
           <Route path="/login" element={<LoginPage />} />
-          <Route path="/SearchList" element={<SearchList />} />
+          <Route path="/searchlist" element={<SearchList />} />
+          <Route path="/search" element={<Search />} />
         </Routes>
       </Layout>
     </LoginContext.Provider>
