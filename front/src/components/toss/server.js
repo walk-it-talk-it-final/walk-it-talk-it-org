da354019e5809e1100d2a759a056f203e1e12a01
kimdabin <dabangzzang@kakao.com>
2024-05-30 23:38:10 +0900

    토스 결제위젯 (추후 백으로 이동 시킬 파일들)

---
+++ front/src/components/toss/server.js
@@ -0,0 +1,46 @@
+
+// routes/index.js 
+require('./toss.js')
+app.use('/payments', router);
+
+// routes/toss.js
+const {confirmPayment} = require('../controller/toss.js');
+
+router.route('/confirm').get(confirmPayment);
+
+// controller/toss.js
+async function confirmPayment(req, res, next) {
+    const paymentInfo = req.query
+    // read-only
+    // TODO: 개발자센터에 로그인해서 내 결제위젯 연동 키 > 시크릿 키를 입력하세요. 시크릿 키는 외부에 공개되면 안돼요.
+    // @docs https://docs.tosspayments.com/reference/using-api/api-keys
+    const secretKey = 'test_gsk_docs_OaPz8L5KdmQXkzRz3y47BMw6';
+    
+    const { paymentKey, orderId, amount } = paymentInfo;
+
+    // 토스페이먼츠 API는 시크릿 키를 사용자 ID로 사용하고, 비밀번호는 사용하지 않습니다.
+    // 비밀번호가 없다는 것을 알리기 위해 시크릿 키 뒤에 콜론을 추가합니다.
+    // @docs https://docs.tosspayments.com/reference/using-api/authorization#%EC%9D%B8%EC%A6%9D
+    const encryptedSecretKey =
+        'Basic ' + Buffer.from(secretKey + ':').toString('base64');
+
+    // ------ 결제 승인 API 호출 ------
+    // @docs https://docs.tosspayments.com/guides/payment-widget/integration#3-결제-승인하기
+    const response = await fetch(
+        'https://api.tosspayments.com/v1/payments/confirm',
+        {
+            method: 'POST',
+            body: JSON.stringify({ orderId, amount, paymentKey }),
+            headers: {
+                Authorization: encryptedSecretKey,
+                'Content-Type': 'application/json',
+            },
+        },
+    );
+    const data = await response.json();
+    console.log(data);
+
+    return res.json({ data: data });
+}
+
+module.exports = { confirmPayment };
