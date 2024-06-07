df9e5a634916dd813fafb759d9571edc1de99a93
kimdabin <dabangzzang@kakao.com>
2024-06-07 09:37:15 +0900

    Feat: 공지사항 등록 구현

--- front/src/components/communities/PostLists.jsx
+++ front/src/components/communities/PostLists.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
-import { Box, Typography, Avatar, IconButton, Select, MenuItem} from '@mui/material';
+import { Box, Typography, Avatar, IconButton } from '@mui/material';
 import CommentIcon from '@mui/icons-material/Comment';
 
 const PostLists = ({ post, onClick }) => {
+    const formatDate = (dateString) => {
+        const options = { year: "numeric", month: "2-digit", day: "2-digit" };
+        return new Date(dateString).toLocaleDateString(undefined, options);
+    };
     return (
         <Box key={post.id} sx={{ mb: 2, maxWidth: "407px" }}>
             <Box
@@ -10,19 +14,19 @@ const PostLists = ({ post, onClick }) => {
                 sx={{ cursor: 'pointer' }}
             >
                 <div style={{ display: "block", border: "1px solid lightgrey", borderRadius: 7, maxWidth: "407px", padding: 10 }}>
-                    <div style={{ display: "flex", justifyreplyContent: "space-between", alignItems: "center" }}>
+                    <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
                         <div style={{ display: "flex", gap: 20, alignItems: "center" }}>
                             <Avatar
                                 alt="프로필 이미지"
-                                src={post.profileImage}
+                                src={post.User.profileImage}
                                 sx={{ width: 50, height: 50, border: "1px solid grey" }}
                             />
                             <div>
-                                <Typography variant="h6" sx={{ fontWeight: "bold" }} id="guestName">
-                                    {post.guestName}
+                                <Typography variant="h6" sx={{ fontWeight: "bold" }} id="nickname">
+                                    {post.User.nickname}
                                 </Typography>
                                 <Typography variant="body1" sx={{ color: 'text.secondary' }} id="commuUploadDate">
-                                    {post.commuUploadDate}
+                                    {formatDate(post.commuUploadDate)}
                                 </Typography>
                             </div>
                         </div>
@@ -36,7 +40,7 @@ const PostLists = ({ post, onClick }) => {
                                 <CommentIcon />
                             </IconButton>
                             <Typography variant="body1" sx={{ color: 'text.secondary', fontWeight: "bold" }} id="comments">
-                                {post.comments.length}
+                                {post.comments ? post.comments.length : 0}
                             </Typography>
                         </div>
                     </div>
